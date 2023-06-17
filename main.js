6c4990b31eb8fe813788e226e1c779e710c69d85
Kushagra Jaiswal <33959544+kushagra105@users.noreply.github.com>
2023-06-17 17:27:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, world! Testing from Github');
+  res.send('Hello, world! Testing from Github #2');
 });
 
 // Start the server
