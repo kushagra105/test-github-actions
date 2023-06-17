1bf13da0e6eda4199572e3894e5ef648e13bbaf3
kushagra105 <jaiswal.kushagra105@gmail.com>
2023-06-17 12:09:57 +0530

    added-files-in-repo

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// Create an instance of the Express app
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
