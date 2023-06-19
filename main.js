20cc0d18d1b1e1bae3685787e7b940f4ffee0974
Kushagra Jaiswal <33959544+kushagra105@users.noreply.github.com>
2023-06-19 20:20:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, world! Testing from Github after halki permissions #5');
+  res.send('Hello, world! Testing from Github Actions #1');
 });
 
 // Start the server
